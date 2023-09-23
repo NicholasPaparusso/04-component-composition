9a15bd33dc6ea0656e8b67baaea60408eb1637eb
NicholasPaparusso <nicopaparusso@gmail.com>
2023-09-23 18:49:14 +0200

    [NEW] using component as wrapper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './assets/style.css';
+import Card from './components/Card';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+            <h1>Todos</h1>
+            
+            <Card>
+              <p>Please note: Below are just the most important todos - feel free to add more.</p>
+            </Card>
+            
+            <ul>
+              <li className="todo">
+                <Card>
+                  <h2>Learn React</h2>
+                  <p>Learn React fundamentals & explore core concepts</p>
+                </Card>
+              </li>
+              <li className="todo">
+                <Card>
+                  <h2>Practice React</h2>
+                  <p>Apply your knowledge & build demo projects</p>
+                </Card>
+              </li>
+            </ul>
+        </div>
     </div>
   );
 }
